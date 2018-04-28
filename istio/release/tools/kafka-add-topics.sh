1372faac6cd4a9ea0b259cf814c9125d2b07ab04
Sehyo Chang <sehyo@nginx.com>
2018-04-28 08:52:00 -0700

    update kafka tools to use better naming convention

--- istio/release/tools/kafka-add-topics.sh
+++ istio/release/tools/kafka-add-topics.sh
@@ -6,5 +6,5 @@ PARTIONS=${2:-1}
 REPLICATION=${3:-1}
 CLIENT=testclient
 KAFKA_NAME=my-kafka
-kubectl -n kafka exec $CLIENT -- /usr/bin/kafka-topics --zookeeper $KAFKA_NAME-zookeeper:2181 \
+kubectl -n kafka exec $CLIENT -- /usr/bin/kafka-topics --zookeeper zookeeper.kafka.svc.cluster.local:2181 \
     --topic $TOPIC_NAME --create --partitions $PARTIONS --replication-factor $REPLICATION
